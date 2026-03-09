2be2d1f8882c99caaff53edd9c28ba51b1db4396
69rem <maksfeed54@gmail.com>
2026-03-09 14:24:30 +0300

    refactor: migrate from NewsAPI to GNews API

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -5,10 +5,11 @@ const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
 export const getNews = async () => {
   try {
-    const response = await axios.get(`${BASE_URL}top-headlines`, {
+    const response = await axios.get(`${BASE_URL}/top-headlines`, {
       params: {
+        lang: "en",
         country: "us",
-        apiKey: API_KEY,
+        apikey: API_KEY,
       },
     });
     return response.data;
