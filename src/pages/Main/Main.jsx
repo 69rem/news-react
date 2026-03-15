1f73368c739812fef090c4a546bc3ce1e9f33472
69rem <maksfeed54@gmail.com>
2026-03-15 19:29:05 +0300

    feat: add design for desktop

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,27 +1,11 @@
 import styles from "./styles.module.css";
-import NewsBanner from "../../components/News-banner/News-banner.jsx";
 import { getNews } from "../../api/apiNews.js";
-import NewsList from "../../components/News-list/News-list.jsx";
-import Pagination from "../../components/Pagination/Pagination.jsx";
-import Categories from "../../components/Categories/Categories.jsx";
-import Search from "../../components/Search/Search.jsx";
 import { useDebounce } from "../../helpers/hooks/useDebounce.js";
-import SearchStatus from "../../components/SearchStatus/SearchStatus.jsx";
-import { PAGE_SIZE, TOTAL_PAGES } from "../../constants/constants.js";
+import { PAGE_SIZE } from "../../constants/constants.js";
 import { useFetch } from "../../helpers/hooks/useFetch.js";
 import { useFilters } from "../../helpers/hooks/useFilters.js";
-
-const CATEGORIES = [
-  "all",
-  "world",
-  "nation",
-  "business",
-  "entertainment",
-  "health",
-  "science",
-  "sports",
-  "technology",
-];
+import LatestNews from "../../components/LatestNews/LatestNews.jsx";
+import NewsByFilters from "../../components/NewsByFilters/NewsByFilters.jsx";
 
 const Main = () => {
   const { filters, changeFilter } = useFilters({
@@ -37,63 +21,16 @@ const Main = () => {
     keywords: debouncedKeywords,
   });
 
-  const handleNextPage = () => {
-    if (filters.page < TOTAL_PAGES) {
-      changeFilter("page", filters.page + 1);
-    }
-  };
-
-  const handlePreviousPage = () => {
-    if (filters.page > 1) {
-      changeFilter("page", filters.page - 1);
-    }
-  };
-
-  const handlePageClick = (pageNumber) => changeFilter("page", pageNumber);
-
   return (
     <main className={styles.main}>
-      <Categories
-        categories={CATEGORIES}
-        setSelectedCategory={(category) => changeFilter("category", category)}
-        selectedCategory={filters.category}
-      />
+      <LatestNews isLoading={isLoading} banners={data && data.articles} />
 
-      <Search
-        keywords={filters.keywords}
-        setKeywords={(keywords) => changeFilter("keywords", keywords)}
-      />
-
-      <SearchStatus
-        keywords={debouncedKeywords}
-        newsLength={data?.articles?.length ?? 0}
+      <NewsByFilters
+        articles={data?.articles}
         isLoading={isLoading}
+        filters={filters}
+        changeFilter={changeFilter}
       />
-
-      <NewsBanner
-        item={data && data.articles ? data.articles[0] : null}
-        isLoading={isLoading}
-      />
-
-      <Pagination
-        handleNextPage={handleNextPage}
-        handlePreviousPage={handlePreviousPage}
-        handlePageClick={handlePageClick}
-        totalPages={TOTAL_PAGES}
-        currentPage={filters.page}
-      />
-
-      <NewsList news={data?.articles} isLoading={isLoading} />
-
-      {(data?.articles?.length ?? 0) > 0 && (
-        <Pagination
-          handleNextPage={handleNextPage}
-          handlePreviousPage={handlePreviousPage}
-          handlePageClick={handlePageClick}
-          totalPages={TOTAL_PAGES}
-          currentPage={filters.page}
-        />
-      )}
     </main>
   );
 };
